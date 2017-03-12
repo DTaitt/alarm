5cb704a70a8702d52c902192b91fd2ff5fcc321e
DTaitt <Donovan.taitt@yahoo.com>
2017-03-12 15:19:33 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+
+// this updates the display time every second
+	setInterval(updateDisplayTime, 1000);
+// this updates the current time every second
+	setInterval(updateCurrentTime, 1000)
+	
+	var globalCurrentTime = [];
+
+
+// this updates the current time and pushes it to the globalCurrentTime array so that it can be used by the checkTime function
+	function updateCurrentTime () {
+
+		globalCurrentTime = [];
+
+		var date = new Date();
+
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			var seconds = date.getSeconds();
+			var year = date.getFullYear();
+			var month = date.getMonth() + 1;
+			var day = date.getDate();
+			var ampm = "AM";
+
+			if (month < 10) {
+				month = "0" + month;
+			}
+
+			if (hours > 11) {
+				ampm = "PM";
+			}
+
+			if (hours > 12) {
+				hours -=12;
+			}
+
+			if(minutes < 10) {
+				minutes = "0" + minutes;
+			}
+
+			if (seconds < 10) {
+				seconds = "0" + seconds;
+			}
+
+			if (ampm === "PM") {
+				hours += 12;
+			}
+
+	//this displays the current time on a 24 hour clock
+
+		var currentTime = year + "-" + month + "-" + day + "T" + hours + ":" + minutes;
+
+		globalCurrentTime.push(currentTime);
+
+	}
+
+// this function updates the display time on the clock 
+
+	function updateDisplayTime() {
+		var date = new Date();
+
+		var hours = date.getHours();
+		var minutes = date.getMinutes();
+		var seconds = date.getSeconds();
+		var year = date.getFullYear();
+		var month = date.getMonth() + 1;
+		var day = date.getDate();
+		var ampm = "AM";
+
+		if (hours > 11) {
+			ampm = "PM";
+		}
+
+		if (hours > 12) {
+			hours -=12;
+		}
+
+		if(minutes < 10) {
+			minutes = "0" + minutes;
+		}
+
+		if (seconds < 10) {
+			seconds = "0" + seconds;
+		}
+
+		var displayTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
+
+		$("#displayTime").html(displayTime)
+	}
+
+// this function sets the alarm time and the check time function checks to see when it equals the alarm time
+
+	function setAlarm() {
+
+		//this is the alarm time on a 24 hour clock
+		var alarmTime = $("#alarmTimeInput").val()
+
+		setInterval(checkTime, 1000);
+
+		function checkTime () {
+
+			if (alarmTime === globalCurrentTime[0]) {
+				$("#alarmTone")[0].play();
+			}
+
+		}
+		
+	}
+
+// this function stops the audio
+	function stopAudio() {
+		$("#alarmTone")[0].pause();
+		
+		setInterval(resetAudio, 1)
+
+		function resetAudio () {
+			$("#alarmTone")[0].currentTime = 0;
+		}
+		
+	}
+
+
+	$("#alarmTimeSave").click(setAlarm)
+	$("#snooze").click(stopAudio)
+
+});
